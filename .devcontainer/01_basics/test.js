1096461d3a3c18800e330cd3356ba21bd5eb9f2b
DRITHIKA <drith0928@gmail.com>
2025-02-23 11:51:32 +0530

    conversions of data types

--- .devcontainer/01_basics/test.js
+++ .devcontainer/01_basics/test.js
@@ -1,1 +1,9 @@
 console.log("drithika")
+//functions
+let score=33
+console.log(typeof score);
+let scores ="33"
+let value=Number(scores)// type converter
+console.log(typeof value);
+//"33"=33:  "33abc"=NaN;   "true"=1
+

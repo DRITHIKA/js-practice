94488cf18aa3ddd0e9f1daa292c8a0e5b911fbde
DRITHIKA <drith0928@gmail.com>
2025-02-23 12:15:21 +0530

    comparisions

--- .devcontainer/01_basics/test.js
+++ .devcontainer/01_basics/test.js
@@ -1,9 +1,20 @@
 console.log("drithika")
 //functions
-let score=33
-console.log(typeof score);
-let scores ="33"
-let value=Number(scores)// type converter
-console.log(typeof value);
+// let score=33
+// console.log(typeof score);
+// let scores ="33"
+// let value=Number(scores)// type converter
+// console.log(typeof value);
 //"33"=33:  "33abc"=NaN;   "true"=1
+//console.log(null>0);== false
+//console.log(null==0)==false
+//console.log(null>=0);== true
+//hdug
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined<0)
+
+
+
+
 

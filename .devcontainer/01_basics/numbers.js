c5b9913930e57ed765d9c5a8c6ae6b9d8c526146
DRITHIKA <drith0928@gmail.com>
2025-02-26 17:03:03 +0530

    math and num

---
+++ .devcontainer/01_basics/numbers.js
@@ -0,0 +1,27 @@
+const score=100
+console.log(score);
+const balance= new Number(100)
+console.log(balance);
+
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));//decimal to 2 finxed numbers
+
+
+const othernumber=23.7688
+console.log(othernumber.toPrecision(3))
+
+const hundereds= 1000000
+console.log(hundereds.toLocaleString('en-IN'))
+
+
+//--------------maths--------------
+console.log(Math)//gives math as an object
+console.log(Math.abs(-2));//gives 2
+
+console.log(Math.round(2.3))
+console.log(Math.ceil(4.2))
+console.log(Math.floor(4.5))
+console.log(Math.min(2,4,3,1))
+//max
+console.log(Math.random())// comes bet 0-1

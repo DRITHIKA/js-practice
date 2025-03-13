393924b5327c6130c21cbadc61022c250eb732c6
DRITHIKA <drith0928@gmail.com>
2025-03-13 13:00:46 +0530

    sksks

---
+++ .devcontainer/01_basics/scope.js
@@ -0,0 +1,12 @@
+function one(){
+    const username ="drithika"
+     
+    function two(){
+        const website ="youtubw"
+        console.log(username);
+        
+    }
+    //console.log(website);
+    two()
+}
+one() 
